e3e2fd56fb6d089ed0b176eef9c6c43c64afb1f0
Ken <ryondc@gmail.com>
2018-09-14 11:29:17 +0700

    add all files

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+    entry:path.resolve(__dirname,'src') + '/app/index.js',
+    output:{
+        path:path.resolve(__dirname,'dist')+'/app',
+        filename:'bundle.js',
+        publicPath:'/app/',
+    },
+    module:{
+        loader:[
+            {
+                test:/\.js$/,
+                include:path.resolve(__dirname,'src'),
+                loader:'babel-loader',
+                query:{
+                    presets:['react','es2015']
+                }
+            },
+        ]
+    }
+};
